976286d14cad3c505a98d765f226706978a1f2d3
cristina1084 <cristina.johns1084@gmail.com>
2019-09-06 10:28:43 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { AddbookComponent } from './addbook/addbook.component';
+import { AuthordetailComponent } from './authordetail/authordetail.component';
+import { AuthorsComponent } from './authors/authors.component';
+import { BooksComponent } from './books/books.component';
+import { EditbookComponent } from './editbook/editbook.component';
+import { IndexComponent } from './index/index.component';
+import { ReadmoreComponent } from './readmore/readmore.component';
+import { UpdatebookComponent } from './updatebook/updatebook.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent,
+    AddbookComponent,
+    AuthordetailComponent,
+    AuthorsComponent,
+    BooksComponent,
+    EditbookComponent,
+    IndexComponent,
+    ReadmoreComponent,
+    UpdatebookComponent
   ],
   imports: [
     BrowserModule,
