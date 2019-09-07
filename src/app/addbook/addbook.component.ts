0815666f0f2060424c291f4267505c9dd76d569e
cristina1084 <cristina.johns1084@gmail.com>
2019-09-07 17:30:49 +0530

    add/edit

--- src/app/addbook/addbook.component.ts
+++ src/app/addbook/addbook.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from "@angular/router";
 import { LibraryService } from "../library.service";
+import {  FileUploader, FileSelectDirective } from 'ng2-file-upload/ng2-file-upload';
+
+const URL = 'http://localhost:8080/books/add';
 
 @Component({
   selector: 'app-addbook',
@@ -12,12 +15,19 @@ export class AddbookComponent implements OnInit {
   constructor(private router: Router, private route: ActivatedRoute, private library: LibraryService) { }
 
   book = {};
+  file;
+  public uploader: FileUploader = new FileUploader({ url: URL, itemAlias: 'bimage' });
 
   ngOnInit() {
+    this.uploader.onAfterAddingFile = (file) => { file.withCredentials = false; };
+    this.uploader.onCompleteItem = (item: any, response: any, status: any, headers: any) => {
+         console.log('ImageUpload:uploaded:', item, status, response);
+    };
   }
 
   addBook(){
     console.log(this.book);
+    this.book['urlToImage'] = this.file.slice(12);
     this.library.addNewBook(this.book).subscribe();
     this.router.navigateByUrl("/main/books");
   }
